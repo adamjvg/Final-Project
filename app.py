63ac0192d99ea7fc8a5752bfb77b14fa0fd28ed5
Angadjeet Sanghera <angadjeet.sanghera@gmail.com>
2022-06-01 21:23:13 +0800

    Flask Two

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #################################################
 # Import Dependencies
 #################################################
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import TextField, SubmitField
+from wtforms.validators import NumberRange
 from tensorflow.keras.models import load_model
 import numpy as np
 from flask_cors import CORS
@@ -18,18 +19,20 @@ bot_model = load_model('bot_trained.h5')
 #################################################
 # Define prediction function
 #################################################
-def return_prediction(model, sample_json):
-    acct_age = sample_json['Account Age']
-    no_fwers = sample_json['no_follower']
-    no_fwing = sample_json['no_following']
-    no_twts = sample_json['no_tweets']
+def return_prediction(model, user_obj):
+    acct_age = user_obj['account_age']
+    no_fwers = user_obj['no_follower']
+    no_fwing = user_obj['no_following']
+    no_twts = user_obj['no_tweets']
+    no_rtwts = user_obj['no_retweets']
 
-    bot_or_not = [[acct_age, no_fwers, no_fwing, no_twts]]
+    bot_or_not = [[acct_age, no_fwers, no_fwing, no_twts, no_rtwts]]
 
     classes = np.array(['spammer', 'non-spammer'])
     class_ind = model.predict_classes(bot_or_not)
 
     return classes[class_ind][0]
+
 
 #################################################
 # Flask Setup
@@ -48,7 +51,8 @@ class UserData(FlaskForm):
     no_follower = TextField('No of Followers')
     no_following = TextField('No Following')
     no_tweets = TextField('No of Tweets')
-    submit = SubmitField('Analyze')
+    no_retweets = TextField('No of Retweets')
+    submit = SubmitField('Analyse')
 
 
 #################################################
@@ -65,22 +69,24 @@ def index():
         session['no_follower'] = form.no_follower.data
         session['no_following'] = form.no_following.data
         session['no_tweets'] = form.no_tweets.data
+        session['no_retweets'] = form.no_retweets.data
 
-    return redirect(url_for('prediction'))
+        return redirect(url_for("prediction"))
 
-    return render_template('home.html', form=form)
+    return render_template('index.html', form = form)
 
 @app.route('/prediction')
 def prediction():
     # Defining content dictionary
     content = {}
 
-    content['Account Age'] = float(session['account_age'])
-    content['No of Followers'] = float(session['no_follower'])
-    content['No Following'] = float(session['no_following'])
-    content['No of Tweets'] = float(session['no_tweets'])
+    content['account_age'] = float(session['account_age'])
+    content['no_follower'] = float(session['no_follower'])
+    content['no_following'] = float(session['no_following'])
+    content['no_tweets'] = float(session['no_tweets'])
+    content['no_retweets'] = float(session['no_retweets'])
 
-    results = return_prediction(model = bot_model, sample_json = content)
+    results = return_prediction(model = bot_model, user_obj = content)
 
     return render_template('prediction.html', results = results)
 
